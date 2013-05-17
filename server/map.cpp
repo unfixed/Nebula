02089f40cec8f9aad9a0820db4d20ad03b801d6a
UnFixed <unfixed@liquefiedlogic.com>
2013-05-17 12:51:24 -0400

    modified map class to list star systems, not players

--- server/map.cpp
+++ server/map.cpp
@@ -1,14 +1,14 @@
 #include "map.h"
 
-Map::Map(int id, const std::vector<int>& input_player_list, int input_size_x, int input_size_y)
+Map::Map(int id, const std::vector<int>& input_starsystem_list, int input_size_x, int input_size_y)
 {
-  setPlayerList(input_player_list);
+  setStarSystemList(input_starsystem_list);
   setSize(input_size_x, input_size_y);
 }
 
-void Map::getPlayerList(std::vector<int>& output_player_list)
+void Map::getStarSystemList(std::vector<int>& output_starsystem_list)
 {
-  output_player_list = player_list;
+  output_starsystem_list = starsystem_list;
 }
 
 void Map::getSize(int& input_size_x, int& input_size_y)
@@ -17,9 +17,9 @@ void Map::getSize(int& input_size_x, int& input_size_y)
   input_size_y = size_y;
 }
 
-void Map::setPlayerList(const std::vector<int>& input_player_list)
+void Map::setStarSystemList(const std::vector<int>& input_starsystem_list)
 {
-  player_list = input_player_list;
+  starsystem_list = input_starsystem_list;
 }
 
 void Map::setSize(int input_size_x, int input_size_y);
--- server/map.h
+++ server/map.h
@@ -6,12 +6,12 @@ class Map : public GameObject
 
 {
 public:
-  Map(int id, const std::vector<int>& input_player_list, int size_x, int size_y);
+  Map(int id, const std::vector<int>& input_starsystem_list, int size_x, int size_y);
 
-  void getPlayerList(std::vector<int>& output_player_list);
+  void getStarSystemList(std::vector<int>& output_starsystem_list);
   void getSize(int& input_size_x, int& input_size_y);
 
-  void setPlayerList(const std::vector<int>& input_player_list);
+  void setStarSystemList(const std::vector<int>& input_starsystem_list);
   void setSize(int input_size_x, int input_size_y);
 
 private:
@@ -19,6 +19,6 @@ private:
 
   int size_x;
   int size_y;
-  std::vector<int> player_list;
+  std::vector<int> starsystem_list;
 
 };
