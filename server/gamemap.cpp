a81e68f46cef586b2e2fa419b413f72b150bd1bd
UnFixed <unfixed@liquefiedlogic.com>
2013-06-03 14:45:35 -0400

    modified gamemap class

--- server/gamemap.cpp
+++ server/gamemap.cpp
@@ -1,41 +1,42 @@
-#include "map.h"
+#include "gamemap.h"
+using namespace std;
 
-Map::Map(int id, int input_size_x, int input_size_y)
+GameMap::GameMap(int id, int input_size_x, int input_size_y)
 {
   setSize(input_size_x, input_size_y);
-  generateMap();
+  generateGameMap();
 }
 
-void Map::getStarSystemList(std::map<StarSystem>& output_starsystem_list)
+void GameMap::getStarSystemList(std::map<int,StarSystem>& output_starsystem_list)
 {
   output_starsystem_list = starsystem_list;
 }
 
-void Map::getSize(int& input_size_x, int& input_size_y)
+void GameMap::getSize(int& input_size_x, int& input_size_y)
 {
   input_size_x = size_x;
   input_size_y = size_y;
 }
 
-void Map::setStarSystemList(const std::map<StarSystem>& input_starsystem_list)
+void GameMap::setStarSystemList(const std::map<int,StarSystem>& input_starsystem_list)
 {
   starsystem_list = input_starsystem_list;
 }
 
-void Map::setSize(int input_size_x, int input_size_y);
+void GameMap::setSize(int input_size_x, int input_size_y)
 {
   size_x = input_size_x;
   size_y = input_size_y;
 }
 
-void Map::addStarSystem(StarSystem& input_starsystem)
+void GameMap::addStarSystem(StarSystem& input_starsystem)
 {
   starsystem_list.insert(std::pair<int,StarSystem>(input_starsystem.getID(),input_starsystem));
 }
 
-void Map::generateMap()
+void GameMap::generateGameMap()
 {
-  //TODO: create algorithm for map generation
+  //TODO: create algorithm for GameMap generation
   
 }
 
